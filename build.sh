c9fa30ef0806cc21d0240730c04e6955dfbdd279
acuicultor <ramoncuenca4@gmail.com>
2018-04-28 21:44:54 +0200

    Build.sh: add build host
    Change-Id: Ib00e0a09e8efc3e90fde57bf61df7b796c32eef1

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export PATH=$PATH:/home/moepda/dup/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
+export CROSS_COMPILE=aarch64-linux-android-
+export KBUILD_BUILD_HOST=unicornblood
+mkdir out
+make ARCH=arm64 O=out merge_kirin970_defconfig
+make ARCH=arm64 O=out -j8
